1686b2f9e0f1dd359a202bebf74681e6c0fcd84a
Rodrigo Paes <rodrigobpaes@gmail.com>
2016-06-17 10:43:43 -0300

    Find max

---
+++ find_max.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find_max(int a[], int n)
+{
+	int m = a[0];
+	for (int i=1; i< n; ++i)
+	{
+		if (a[i] > m ) 
+			m = a[i];
+	}
+	return m;
+}
+
+int main()
+{
+	int x[] = {3,4,46,34,5};
+	printf("%d\n", find_max(x, 5));
+	return 0;
+}
