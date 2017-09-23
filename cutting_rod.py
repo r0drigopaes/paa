aa850703dbb608f0fd255841b946d8c2c837ceca
Leandro97 <leandro97.martins@gmail.com>
2017-09-23 16:22:16 -0300

    Programação Dinâmica - corte de hastes

--- cutting_rod.py
+++ cutting_rod.py
@@ -1,41 +1,46 @@
 def naive(length):
-  best = -1
-  for i in range(length):
-    best = best if (best > prices[i] + topDown(length - i - 1)) else prices[i] + topDown(length - i - 1)
-  return best
+	if (length == 0): return 0
+	
+	best = -1;
+	for i in range(length):
+	  aux = prices[i] + naive(length - i - 1)
+	  best = best if (best > aux) else aux
+	return best
 
-def topDown(length):
+def topDown_dp(length):
   if(memo[length] != -1):
     return memo[length]
   
   best = -1
   for i in range(length):
-    best = best if (best > prices[i] + topDown(length - i - 1)) else prices[i] + topDown(length - i - 1)
+    aux = prices[i] + topDown_dp(length - i - 1)
+    best = best if (best > aux) else aux
   
   memo[length] = best
   return memo[length]
 
-def bottomUp(length):
-  for i in range(length):
+def bottomUp_dp(length):
+  for i in range(1, length + 1):
     best = -1
-    for j in range(i + 1):
-      best = best if (best > prices[i] + memo[j - i - 1]) else prices[i] + memo[j - i - 1]
+    for j in range(i):
+      aux = prices[j] + memo[i - j - 1]
+      best = best if (best > aux) else aux
     memo[i] = best
   
   return memo[length] 
   
-rodLength = input()
-while(rodLength != ''):
-  iRodLength = int(rodLength)
+rodLength = int(input())
+while(rodLength != 0):
   prices = []
   memo = []
   
-  prices.extend(list(map(int, input().split())))
+  for i in range(rodLength):
+    prices.append(int(input()))
   
   memo.append(0)
-  memo.extend([-1 for x in range(iRodLength)])
+  memo.extend([-1 for x in range(rodLength)])
 
-  #print(naive(iRodLength))
-  #print(topDown(iRodLength))
-  #print(bottomUp(iRodLength))
-  rodLength = input()
+  #print(naive(rodLength))
+  #print(topDown_dp(rodLength))
+  #print(bottomUp_dp(rodLength))
+  rodLength = int(input())
